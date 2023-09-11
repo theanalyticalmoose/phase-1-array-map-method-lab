521b865e6a32665090372302b2cc97339f711413
Rachael <rachael.brady@theanalyticalmoose.com>
2023-09-10 18:58:27 -0700

    complete

--- index.js
+++ index.js
@@ -1,16 +1,43 @@
+
 const tutorials = [
-  'what does the this keyword mean?',
-  'What is the Constructor OO pattern?',
-  'implementing Blockchain Web API',
-  'The Test Driven Development Workflow',
-  'What is NaN and how Can we Check for it',
-  'What is the difference between stopPropagation and preventDefault?',
-  'Immutable State and Pure Functions',
-  'what is the difference between == and ===?',
-  'what is the difference between event capturing and bubbling?',
-  'what is JSONP?'
+  "what does the this keyword mean?",
+  "what is the Constructor OO pattern?",
+  "implementing blockchain web API",
+  "the test driven development workflow",
+  "what is NaN and how can we check for it",
+  "what is the difference between stopPropagation and preventDefault?",
+  "immutable state and pure functions",
+  "what is the difference between == and ===?",
+  "what is the difference between event capturing and bubbling?",
+  "what is JSONP?"
 ];
 
-const titleCased = () => {
-  return tutorials
+function titleCased() {
+  return tutorials.map(tutorial => {
+    let words = tutorial.split(' ');
+    return words.map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(' ');
+  });
 }
+
+describe('index.js', function () {
+  describe('titleCased()', function () {
+    it('returns an array with title case tutorial names', function () {
+      expect(titleCased()).to.have.all.members(
+        [
+          "What Does The This Keyword Mean?",
+          "What Is The Constructor OO Pattern?",
+          "Implementing Blockchain Web API",
+          "The Test Driven Development Workflow",
+          "What Is NaN And How Can We Check For It",
+          "What Is The Difference Between StopPropagation And PreventDefault?",
+          "Immutable State And Pure Functions",
+          "What Is The Difference Between == And ===?",
+          "What Is The Difference Between Event Capturing And Bubbling?",
+          "What Is JSONP?"
+        ]
+      )
+    });
+  });
+});
+
+
